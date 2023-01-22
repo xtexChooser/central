878bca2be3c51b034cb0106ea47d1874830124b8
Kunal Mehta <legoktm@debian.org>
2023-01-22 02:03:17 -0500

    Add new de-linting bot

---
+++ src/font.rs
@@ -0,0 +1,215 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// (C) Copyright 2023 Kunal Mehta <legoktm@debian.org>
+enum Mode {
+    RelativePlus,
+    RelativeMinus,
+    Absolute,
+}
+
+/// Implements the "rules for parsing a legacy font size"
+/// https://html.spec.whatwg.org/multipage/rendering.html#phrasing-content-3
+pub(crate) fn parse_legacy_font_size(input: &str) -> Option<&'static str> {
+    // 1. Let input be the attribute's value.
+    // 2. Let position be a pointer into input, initially pointing at the start of the string.
+    // 3. Skip ASCII whitespace within input given position.
+    //    (yes, we check Unicode whitespace, shhhh)
+    let input = input.trim_start();
+    // 4. If position is past the end of input, there is no presentational hint. Return.
+    if input.is_empty() {
+        return None;
+    }
+    let mut pos = 0;
+    // 5. If the character at position is a U+002B PLUS SIGN character (+), then let mode be
+    //    relative-plus, and advance position to the next character. Otherwise, if the character
+    //    at position is a U+002D HYPHEN-MINUS character (-), then let mode be relative-minus,
+    //    and advance position to the next character. Otherwise, let mode be absolute.
+    let mode = match input.chars().next() {
+        Some('+') => {
+            pos += 1;
+            Mode::RelativePlus
+        }
+        Some('-') => {
+            pos += 1;
+            Mode::RelativeMinus
+        }
+        _ => Mode::Absolute,
+    };
+    // 6. Collect a sequence of code points that are ASCII digits from input given position, and
+    //    let the resulting sequence be digits.
+    let mut digits = "".to_string();
+    for c in input.chars().skip(pos) {
+        if matches!(c, '0'..='9') {
+            digits.push(c);
+        } else {
+            break;
+        }
+    }
+    // 7. If digits is the empty string, there is no presentational hint. Return.
+    if digits.is_empty() {
+        return None;
+    }
+    // 8. Interpret digits as a base-ten integer. Let value be the resulting number.
+    let mut value: isize = match digits.parse() {
+        Ok(value) => value,
+        Err(_) => {
+            return None;
+        }
+    };
+    // 9. If mode is relative-plus, then increment value by 3. If mode is relative-minus, then
+    //    let value be the result of subtracting value from 3.
+    match mode {
+        Mode::RelativePlus => {
+            value += 3;
+        }
+        Mode::RelativeMinus => {
+            value -= 3;
+        }
+        Mode::Absolute => {}
+    }
+    // 10. If value is greater than 7, let it be 7.
+    // 11. If value is less than 1, let it be 1.
+    value = value.clamp(1, 7);
+    // 12. Set 'font-size' to the keyword corresponding to the value of value according
+    //     to the following table:
+    let keyword = match value {
+        1 => "x-small",
+        2 => "small",
+        3 => "medium",
+        4 => "large",
+        5 => "x-large",
+        6 => "xx-large",
+        7 => "xxx-large",
+        val => unreachable!("got value of {}", val),
+    };
+    Some(keyword)
+}
+
+#[test]
+fn test_parse_legacy_font_size() {
+    assert_eq!(parse_legacy_font_size("2"), Some("small"));
+    assert_eq!(parse_legacy_font_size("+2"), Some("x-large"));
+    assert_eq!(parse_legacy_font_size("-8"), Some("x-large"));
+    assert_eq!(parse_legacy_font_size("3px"), Some("medium"));
+    assert_eq!(parse_legacy_font_size("ahhhh"), None);
+    assert_eq!(parse_legacy_font_size("-"), None);
+}
+
+/// Implements the "rules for parsing a legacy color value"
+/// https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#rules-for-parsing-a-legacy-colour-value
+pub(crate) fn parse_legacy_color_value(input: &str) -> Option<String> {
+    // 1. Let input be the string being parsed.
+    // 2. If input is the empty string, then return an error.
+    if input.is_empty() {
+        return None;
+    }
+    // 3. Strip leading and trailing ASCII whitespace from input.
+    //    (yes, we check Unicode whitespace, shhhh)
+    let input = input.trim();
+    // 4. If input is an ASCII case-insensitive match for the string "transparent", then return
+    //     an error.
+    if input.to_ascii_lowercase() == "transparent" {
+        return None;
+    }
+    // 5. If input is an ASCII case-insensitive match for one of the named colors, then return
+    //     the simple color corresponding to that keyword.
+    if crate::colors::NAMED_COLORS
+        .contains(&input.to_ascii_lowercase().as_str())
+    {
+        return Some(input.to_ascii_lowercase());
+    }
+    // 6. If input's code point length is four, and the first character in input is U+0023 (#),
+    //    and the last three characters of input are all ASCII hex digits, then:
+    //    1. Let result be a simple color.
+    //    ...
+    //    5. Return result.
+    if input.len() == 4 {
+        let mut chars = input.chars();
+        if chars.next() == Some('#') && chars.all(is_ascii_hex_digit) {
+            return Some(input.to_string());
+        }
+    }
+    // 7. Replace any code points greater than U+FFFF in input (i.e., any characters that are
+    //     not in the basic multilingual plane) with the two-character string "00".
+    //    (assume this doesn't apply to us)
+    // 8. If input's code point length is greater than 128, truncate input, leaving only the
+    //    first 128 characters.
+    let mut input = input.to_string();
+    if input.len() > 128 {
+        input.truncate(128);
+    }
+    // 9. If the first character in input is a U+0023 NUMBER SIGN character (#), remove it.
+    if input.starts_with('#') {
+        input = input.strip_prefix('#').unwrap().to_string();
+    }
+    // 10. Replace any character in input that is not an ASCII hex digit with the character
+    //     U+0030 DIGIT ZERO (0).
+    let mut input: String = input
+        .chars()
+        .map(|c| if is_ascii_hex_digit(c) { c } else { '0' })
+        .collect();
+    // 11. While input's code point length is zero or not a multiple of three, append a
+    //     U+0030 DIGIT ZERO (0) character to input.
+    while input.len() % 3 != 0 {
+        input.push('0');
+    }
+    // 12. Split input into three strings of equal code point length, to obtain three
+    //     components. Let length be the code point length that all of those components
+    //     have (one third the code point length of input).
+    let mut length = input.len() / 3;
+    let (first, remaining) = input.split_at(length);
+    let (second, third) = remaining.split_at(length);
+    let mut first = first.to_string();
+    let mut second = second.to_string();
+    let mut third = third.to_string();
+    // 13. If length is greater than 8, then remove the leading length-8 characters in
+    //     each component, and let length be 8.
+    if length > 8 {
+        let strip = length - 8;
+        first = strip_chars(first, strip);
+        assert_eq!(first.len(), 8);
+        second = strip_chars(second, strip);
+        third = strip_chars(third, strip);
+        length = 8;
+    }
+    // 14. While length is greater than two and the first character in each component is
+    //     a U+0030 DIGIT ZERO (0) character, remove that character and reduce length by one.
+    while length > 2
+        && first.starts_with('0')
+        && second.starts_with('0')
+        && third.starts_with('0')
+    {
+        first = strip_chars(first, 1);
+        second = strip_chars(second, 1);
+        third = strip_chars(third, 1);
+        length -= 1;
+    }
+    // 15. If length is still greater than two, truncate each component, leaving only
+    //     the first two characters in each.
+    if length > 2 {
+        first.truncate(2);
+        second.truncate(2);
+        third.truncate(2);
+    }
+    // 16. Let result be a simple color.
+    Some(format!("#{first}{second}{third}"))
+}
+
+/// Strip the count number of characters from the left
+fn strip_chars(input: String, count: usize) -> String {
+    input.chars().skip(count).collect()
+}
+
+/// https://infra.spec.whatwg.org/#ascii-hex-digit
+fn is_ascii_hex_digit(c: char) -> bool {
+    matches!(c, '0'..='9' | 'A'..='F' | 'a'..='f')
+}
+
+#[test]
+fn test_parse_legacy_color_value() {
+    assert_eq!(parse_legacy_color_value("transparent"), None);
+    assert_eq!(parse_legacy_color_value("black"), Some("black".to_string()));
+    assert_eq!(parse_legacy_color_value("#000"), Some("#000".to_string()));
+    // User talk:Patmax23
+    assert_eq!(parse_legacy_color_value("coal"), Some("#c0a000".to_string()));
+    // FIXME: more test cases
+}
---
+++ src/main.rs
@@ -0,0 +1,238 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// (C) Copyright 2023 Kunal Mehta <legoktm@debian.org>
+mod colors;
+mod font;
+
+use anyhow::Result;
+use kuchiki::NodeRef;
+use mwbot::parsoid::prelude::*;
+use mwbot::{Bot, Page};
+use serde::Deserialize;
+use std::collections::{HashMap, HashSet};
+use tokio::fs;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let bot = Bot::from_default_config().await?;
+    let pages = bot
+        .api()
+        .get_value(HashMap::from([
+            ("action", "query"),
+            ("list", "linterrors"),
+            ("lntcategories", "obsolete-tag"),
+            ("lntlimit", "max"),
+        ]))
+        .await?;
+    let mut seen = HashSet::new();
+    for error in pages["query"]["linterrors"].as_array().unwrap() {
+        let title = error["title"].as_str().unwrap().to_string();
+        if !seen.contains(&title) {
+            // TODO: should we check and skip on templateInfo?
+            let page = bot.page(&title)?;
+            process_page(&bot, page).await?;
+            seen.insert(title);
+        }
+    }
+    Ok(())
+}
+
+async fn process_page(bot: &Bot, page: Page) -> Result<()> {
+    println!("Checking {}...", page.title());
+    let page_id = page.id().await?.expect("page doesn't exist");
+    let original_html = page.html().await?;
+    let html = handle_page(original_html.clone())?;
+    let original = page.wikitext().await?;
+    let new_text = bot.parsoid().transform_to_wikitext(&html).await?;
+    let remaining = lint_errors(bot, page.title(), &new_text).await?;
+    if remaining.iter().any(|lint| lint.type_ == "obsolete-tag") {
+        println!(
+            "{} still has obsolete-tag lint errors, skipping",
+            page.title()
+        );
+        return Ok(());
+    }
+    if original == new_text {
+        // In theory this should still trip lint errors, but double check just in case
+        println!("No changes to {}, skipping", page.title());
+        return Ok(());
+    }
+    fs::write(format!("output/html/{page_id}_original.html"), original_html.html())
+        .await?;
+    fs::write(format!("output/html/{page_id}_modified.html"), html.html())
+        .await?;
+    let formatted = include_str!("diff.html")
+        .replace("{diff}", &html_diff(bot, &original, &new_text).await?)
+        .replace("{title}", page.title())
+        .replace("{pageid}", &page_id.to_string());
+
+    fs::write(format!("output/{page_id}_diff.html"), formatted).await?;
+    Ok(())
+}
+
+async fn html_diff(bot: &Bot, left: &str, right: &str) -> Result<String> {
+    let result = bot
+        .api()
+        .post_value(HashMap::from([
+            ("action", "compare"),
+            ("fromtext-main", left),
+            ("totext-main", right),
+            ("fromslots", "main"),
+            ("toslots", "main"),
+            ("fromcontentmodel-main", "wikitext"),
+        ]))
+        .await?;
+    Ok(result["compare"]["body"].as_str().unwrap().to_string())
+}
+
+#[derive(Deserialize)]
+struct LintError {
+    #[serde(rename = "type")]
+    type_: String,
+}
+
+async fn lint_errors(
+    bot: &Bot,
+    title: &str,
+    wikitext: &str,
+) -> Result<Vec<LintError>> {
+    let req = bot.api().http_client().post(
+        format!("https://en.wikipedia.org/api/rest_v1/transform/wikitext/to/lint/{}", urlencoding::encode(title)
+    )).form(&[("wikitext", wikitext)])
+            .build()?;
+    let resp = bot.api().http_client().execute(req).await?.json().await?;
+    Ok(resp)
+}
+
+/// Copy children from the old node to the new one
+fn copy_children(old: &NodeRef, new: &NodeRef) {
+    for child in old.children() {
+        new.append(child);
+    }
+}
+
+/// Replace the old node with the new one
+fn swap_nodes(old: &NodeRef, new: &NodeRef) {
+    old.insert_after(new.clone());
+    old.detach();
+}
+
+/// Copy all attributes from the old node to the new one
+fn copy_attributes(old: &NodeRef, new: &NodeRef) {
+    let mut attrs = new.as_element().unwrap().attributes.borrow_mut();
+    for (name, value) in &old.as_element().unwrap().attributes.borrow().map {
+        attrs.insert(name.local.to_string(), value.value.to_string());
+    }
+}
+
+fn handle_font(font: Wikinode) {
+    let span = Wikicode::new_node("span");
+    let mut style = vec![];
+    // Copy attributes from <font> to <span>
+    for (name, value) in &font.as_element().unwrap().attributes.borrow().map {
+        let attr = name.local.to_string();
+        match attr.as_str() {
+            "color" => {
+                if let Some(color) =
+                    font::parse_legacy_color_value(&value.value)
+                {
+                    style.push(format!("color: {};", color))
+                }
+            }
+            "face" => {
+                // XXX: in theory some should be quoted, but it doesn't seem necessary
+                style.push(format!("font-family: {};", &value.value));
+            }
+            "size" => {
+                if let Some(font_size) =
+                    font::parse_legacy_font_size(&value.value)
+                {
+                    style.push(format!("font-size: {};", font_size));
+                }
+            }
+            // style needs to be merged in with our new styles
+            "style" => style.push(value.value.to_string()),
+            other => {
+                // Pass it back as an attribute on <span>
+                span.as_element()
+                    .unwrap()
+                    .attributes
+                    .borrow_mut()
+                    .insert(other, value.value.to_string());
+            }
+        }
+    }
+    if !style.is_empty() {
+        span.as_element()
+            .unwrap()
+            .attributes
+            .borrow_mut()
+            .insert("style", style.join(" "));
+    }
+    copy_children(&font, &span);
+    swap_nodes(&font, &span);
+}
+
+fn handle_strike(strike: Wikinode) {
+    let s = Wikicode::new_node("s");
+    copy_attributes(&strike, &s);
+    copy_children(&strike, &s);
+    swap_nodes(&strike, &s);
+}
+
+fn handle_tt(tt: Wikinode) {
+    let code = Wikicode::new_node("code");
+    copy_attributes(&tt, &code);
+    copy_children(&tt, &code);
+    swap_nodes(&tt, &code);
+}
+
+fn handle_center(center: Wikinode) {
+    let div = Wikicode::new_node("div");
+    let mut style = vec!["text-align: center".to_string()];
+    // Copy attributes from <center> to <div>
+    for (name, value) in &center.as_element().unwrap().attributes.borrow().map {
+        let attr = name.local.to_string();
+        match attr.as_str() {
+            // style needs to be merged in with our new styles
+            "style" => style.push(value.value.to_string()),
+            other => {
+                // Pass it back as an attribute on <span>
+                div.as_element()
+                    .unwrap()
+                    .attributes
+                    .borrow_mut()
+                    .insert(other, value.value.to_string());
+            }
+        }
+    }
+    if !style.is_empty() {
+        div.as_element()
+            .unwrap()
+            .attributes
+            .borrow_mut()
+            .insert("style", style.join(" "));
+    }
+    copy_children(&center, &div);
+    swap_nodes(&center, &div);
+}
+
+fn handle_page(html: ImmutableWikicode) -> Result<ImmutableWikicode> {
+    let html = html.into_mutable();
+    for font in html.select("font") {
+        println!("found <font>");
+        handle_font(font);
+    }
+    for strike in html.select("strike") {
+        println!("found <strike>");
+        handle_strike(strike);
+    }
+    for tt in html.select("tt") {
+        println!("found <tt>");
+        handle_tt(tt);
+    }
+    for center in html.select("center") {
+        println!("found <center>");
+        handle_center(center);
+    }
+    Ok(html.into_immutable())
+}
