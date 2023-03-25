f0c398b2a23b09903f858f892b173574f7ef5a05
Kunal Mehta <legoktm@debian.org>
2023-03-25 03:01:47 -0400

    Update database schema to make d_title unique
    Already deployed...

--- deferred.sql
+++ deferred.sql
@@ -1,1 +1,4 @@
-create table deferred (d_title varbinary(300));
+CREATE TABLE `deferred` (
+  `d_title` varbinary(300) DEFAULT NULL,
+  UNIQUE KEY `d_title` (`d_title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
