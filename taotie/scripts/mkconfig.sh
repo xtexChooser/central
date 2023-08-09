15a41458d9a43098cb1266b9e0e2aab9da67c175
xtex <xtexchooser@duck.com>
2023-08-09 17:45:07 +0800

    fix(mkconfig): mrproper after mkconfig
    Signed-off-by: xtex <xtexchooser@duck.com>

--- taotie/scripts/mkconfig.sh
+++ taotie/scripts/mkconfig.sh
@@ -5,5 +5,5 @@ cd "$KDIR" && eval $KMAKE_ARGS KCONFIG_CONFIG="$DDIR/defconfig.merge" \
     scripts/kconfig/merge_config.sh \
     "$DDIR/defconfig.base" \
     "taotie/android-configs/u/android-6.1/android-base.config" \
-    "$DDIR/defconfig.ext"
+    "$DDIR/defconfig.ext" && make ARCH=$ARCH mrproper
 rm -f "$DDIR/defconfig.merge.old"
