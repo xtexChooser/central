34c432bac3e776a9cd43d7f22b5dcdf6cc6ebb4e
xtex <xtexchooser@duck.com>
2023-08-11 15:19:59 +0800

    update

---
+++ lougu/P1009.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+__uint128_t S(unsigned long long n) {
+  __uint128_t a, i;
+  a = 1;
+  i = 1;
+  for (; i <= n; i++) {
+    a *= i;
+  }
+  return a;
+}
+
+int main() {
+  unsigned long long n, i = 1;
+  __int128_t sum = 0;
+  scanf("%lld", &n);
+  for (; i <= n; i++) {
+    sum += S(i);
+  }
+  char buf[129];
+  int ptr = 12;
+  buf[128] = 0;
+  while (sum > 0) {
+    buf[ptr] = '0' + (((unsigned int)sum) % 2);
+    sum /= 2;
+    ptr--;
+  }
+  printf("%s\n", &buf[ptr + 1]);
+  return 0;
+}
