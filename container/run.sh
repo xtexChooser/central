cddc41005202255b265fa5e109a02f527ca97e98
xtex <xtexchooser@duck.com>
2023-11-19 11:30:35 +0800

    fix mwbot config
    Signed-off-by: xtex <xtexchooser@duck.com>

--- container/run.sh
+++ container/run.sh
@@ -4,4 +4,5 @@ PATH=/dist/bin:/dist/sbin:$PATH
 
 mkdir -p /srv/exopages/xtex-wiki-bot
 ln -s /srv/exopages/xtex-wiki-bot pub
+ln -s /srv/run/mwbot-mcwzh.toml mwbot-mcwzh.toml
 exec dinit -d /dist/dinit.d --container -t xt-bot
