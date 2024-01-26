62ac141877954708b9ba010344a5a2c41eb48327
xtex <xtexchooser@duck.com>
2024-01-26 21:23:48 +0800

    Add alias for goto and envs.sh

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,9 @@ if status is-interactive
 end
 
 alias g=git
+alias gt=goto
+
+function 0file; curl -F"file=@$argv" https://envs.sh; end
+function 0pb; curl -F"file=@-;" https://envs.sh; end
+function 0url; curl -F"url=$argv" https://envs.sh; end
+function 0short; curl -F"shorten=$argv" https://envs.sh; end
