8cf19acb85140ea84dff5d2f3fabd85a727baece
xtex <xtexchooser@duck.com>
2024-09-06 17:13:44 +0800

    Customize for Minecraft Wiki ZH

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,16 @@
 use anyhow::Result;
 use delinter::{api, delint_html, Options, Summary};
 use mwbot::{Bot, Page, SaveOptions};
-use mysql_async::{prelude::Queryable, Pool};
+// mcwzh: no toolforge database
+// use mysql_async::{prelude::Queryable, Pool};
 use tracing::{debug, error, info};
 
 enum Outcome {
     /// Fixed the page
     Fixed,
     /// Deferred for human review
-    Deferred,
+    // mcwzh: no deferring
+    // Deferred,
     /// Skipped entirely
     Skipped,
 }
@@ -27,18 +29,20 @@ async fn main() -> Result<()> {
         center_gallery: false,
     };
     let bot = Bot::from_default_config().await?;
-    let pool = Pool::new(
-        toolforge::db::toolsdb("s55279__delinterbot_p".to_string())?
-            .to_string()
-            .as_str(),
-    );
+    // mcwzh: no toolforge database
+    // let pool = Pool::new(
+    //     toolforge::db::toolsdb("s55279__delinterbot_p".to_string())?
+    //         .to_string()
+    //         .as_str(),
+    // );
     let mut gen = api::linterror_pages(&bot);
     let mut handles = vec![];
     while let Some(result) = gen.recv().await {
         let page = result?;
         if page.namespace() == 2 {
             // Skip userspace for now
-            continue;
+            // mcwzh: don't skip userspace
+            // continue;
         }
         let bot = bot.clone();
         // Spawn a thread for each page
@@ -47,19 +51,20 @@ async fn main() -> Result<()> {
             (title, process_page(&opts, &bot, page).await)
         }));
         // Once we have 200+ threads, await them all
-        if handles.len() >= 200 {
+        // mcwzh: less threads
+        if handles.len() >= 3 {
             while let Some(handle) = handles.pop() {
                 let (title, result) = handle.await?;
                 match result {
-                    Ok(Outcome::Deferred) => {
-                        info!("Deferring {title} for human review");
-                        let mut conn = pool.get_conn().await?;
-                        conn.exec_drop(
-                            "INSERT IGNORE INTO deferred VALUES(?,?)",
-                            (title, false),
-                        )
-                        .await?;
-                    }
+                    // Ok(Outcome::Deferred) => {
+                    //     info!("Deferring {title} for human review");
+                    //     let mut conn = pool.get_conn().await?;
+                    //     conn.exec_drop(
+                    //         "INSERT IGNORE INTO deferred VALUES(?,?)",
+                    //         (title, false),
+                    //     )
+                    //     .await?;
+                    // }
                     Ok(_) => {}
                     Err(err) => {
                         // Log it and we move on
@@ -88,6 +93,8 @@ async fn process_page(
     let html = delint_html(opts, original_html.clone(), &mut summary, &[])?;
     let original = page.wikitext().await?;
     let new_text = bot.parsoid().transform_to_wikitext(&html).await?;
+    // mcwzh: deal with cloudflare quirks
+    let new_text = regex::Regex::new("<script [^>]* src=\"https:\\/\\/static\\.cloudflareinsights\\.com[^>]*><\\/script>")?.replace_all(&new_text, "").to_string();
     if new_text.matches("<nowiki>").count()
         > original.matches("<nowiki>").count()
     {
@@ -97,26 +104,28 @@ async fn process_page(
     let remaining =
         api::remaining_linterrors(bot, page.title(), &new_text).await?;
     if !remaining.is_empty() {
+        // mcwzh: no human deferring, save edits
         if remaining.iter().all(|l| l.is_human_fixable()) {
+            // info!(
+            //     "{} has human-fixable lint errors remaining, will defer",
+            //     page.title()
+            // );
+            // return Ok(Outcome::Deferred);
+        } else {
+            // Unfixable, skip
+            summary.remaining_lints = remaining;
             info!(
-                "{} has human-fixable lint errors remaining, will defer",
-                page.title()
+                "{} still has some lint errors ({}), will be skipped",
+                page.title(),
+                summary
+                    .remaining_lints
+                    .into_iter()
+                    .map(|l| l.type_)
+                    .collect::<Vec<_>>()
+                    .join(", ")
             );
-            return Ok(Outcome::Deferred);
+            return Ok(Outcome::Skipped);
         }
-        // Unfixable, skip
-        summary.remaining_lints = remaining;
-        info!(
-            "{} still has some lint errors ({}), will be skipped",
-            page.title(),
-            summary
-                .remaining_lints
-                .into_iter()
-                .map(|l| l.type_)
-                .collect::<Vec<_>>()
-                .join(", ")
-        );
-        return Ok(Outcome::Skipped);
     }
     if original == new_text {
         // In theory this should still trip lint errors, but double check just in case
@@ -124,12 +133,23 @@ async fn process_page(
         summary.no_change = true;
         return Ok(Outcome::Skipped);
     }
+    // mcwzh: write logs
+    {
+        use std::fs;
+        fs::create_dir_all(format!("out/{}", page.title()))?;
+        fs::write(format!("out/{}/orig", page.title()), original)?;
+        fs::write(format!("out/{}/new", page.title()), &new_text)?;
+        fs::write(
+            format!("out/{}/summary", page.title()),
+            summary.edit_summary(),
+        )?;
+    }
     info!("Saving {}: {}", page.title(), summary.edit_summary());
     page.save(
         new_text,
-        &SaveOptions::summary(&summary.edit_summary())
-            .mark_as_minor(true)
-            .add_tag("fixed lint errors"),
+        &SaveOptions::summary(&summary.edit_summary()).mark_as_minor(true),
+        // mcwzh: fixing tag could not be added manually
+        // .add_tag("fixed lint errors"),
     )
     .await?;
     Ok(Outcome::Fixed)
