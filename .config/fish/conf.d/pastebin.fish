5c02eb3f26a4d96c85dea77cb04e8480a35c7550
xtex <xtexchooser@duck.com>
2024-10-03 16:03:59 +0800

    fish: fix pastebin

--- .config/fish/conf.d/pastebin.fish
+++ .config/fish/conf.d/pastebin.fish
@@ -1,4 +1,4 @@
-function 0file; curl -F"file=@$1" https://envs.sh; end
+function 0file; curl -F"file=@$argv" https://envs.sh; end
 function 0pb; curl -F"file=@-;" https://envs.sh; end
-function 0url; curl -F"url=$1" https://envs.sh; end
-function 0short; curl -F"shorten=$1" https://envs.sh; end
+function 0url; curl -F"url=$argv" https://envs.sh; end
+function 0short; curl -F"shorten=$argv" https://envs.sh; end
