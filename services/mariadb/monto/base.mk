e039183959ac889dc38b43ff6585433db675df77
xtex <xtexchooser@duck.com>
2025-02-10 21:02:17 +0800

    monto: extend memory limitation to 256M
    Change-Id: Ib54a0c80d5dbd784247af9b992c437aed592e61e

--- services/mariadb/monto/base.mk
+++ services/mariadb/monto/base.mk
@@ -4,7 +4,7 @@ V_DEPS		+= /var/lib/mariadb/monto/my.cnf
 V_DEPS_ORD	+= /var/lib/mariadb/monto
 V_ARGS		+= --mount=type=bind,src=/var/lib/mariadb/monto,dst=/var/lib/mariadb
 V_ARGS		+= --mount=type=bind,src=/var/run/mariadb/monto,dst=/var/run/mariadb
-V_ARGS		+= --memory=128M
+V_ARGS		+= --memory=256M
 V_ARGS		+= --publish=3307:3306/tcp
 V_ARGS 		+= codeberg.org/xens/mariadb:latest
 V_ARGS 		+= --user=root
